5cee5ae23c216fe8c066eb2dbdee3112b5e5fd6d
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2025-06-30 20:41:02 +0530

    connect user button
    make user page and userform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import {useNavigate} from 'react-router-dom';
 import './App.css';
 
 function App() {
+
+const navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
        <h1>Welcome to testApp</h1>
-       <button className='user-button'>User</button>
+       <button className='user-button' onClick={() => navigate('/user')}>User</button>
       </header>
     </div>
   );
